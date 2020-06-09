b54522e854462274081d1ac3929624a53e2ea3cc
meetmmpatel <meetmmpatel@gmail.com>
2020-06-09 13:52:24 -0400

    Added First commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (request, response) => {
+  response.send(`API is Running on port number ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Api is running on port number ${PORT}`);
+});
